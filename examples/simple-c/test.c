331b48ea0ae12003b96ef8772094ed37343a2317
Ties Stuij <ties.stuij@arm.com>
2023-09-25 11:42:48 +0100

    straighten out examples

--- examples/simple-c/test.c
+++ examples/simple-c/test.c
@@ -1,7 +1,9 @@
 void draw_screen() {
+  // set background mode 3 (bitmap) and turn on background 2
   *(unsigned long*) 0x4000000 = 0x403;
 
   unsigned short* vram = (unsigned short*) 0x6000000;
+  // write red to every pixel on the 240x160 screen
   for(int x = 0; x < 240; x++)
     for(int y = 0; y < 160; y++)
       vram[x + y * 240] = 31;
--- examples/simple-cxx/test.cpp
+++ examples/simple-cxx/test.cpp
@@ -1,12 +1,15 @@
 #include <vector>
 
 void draw_screen() {
+  // set background mode 3 (bitmap) and turn on background 2
   *(unsigned long*) 0x4000000 = 0x403;
 
+  // put colors red, green and blue in a vector
   std::vector<int> v = {31};
   v.push_back(31 << 5);
   v.insert(v.end(), 31 << 10);
 
+  // pixel by pixel, paint red, green and blue stripes to the 240x160 screen
   unsigned short* vram = (unsigned short*) 0x6000000;
   for(int x = 0; x < 240; x++)
     for(int y = 0; y < 160; y++)
